ffd54173f0eb2c7394db7f44fc34c46932414aa0
ahmed007ahmed <ahmed736937467@gmail.com>
2026-09-23 04:28:28 +0300

    Initialize build.gradle.kts for Android application

---
+++ app/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.ram.agent"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.ram.agent"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+}
